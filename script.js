90880cdefca1a30c651f7deec17a5fa05bd002b5
심주연 <misssim29@naver.com>
2023-05-18 15:31:49 +0900

    .

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-
+import axios from 'axios';
 
 //단어 입력
 function addWord(){
-    axios({
-        method: 'get',
-        url:'https://stdict.korean.go.kr/api/search.do',
-        params: {
-            key : "9311B28987B0F92D920BE18F75AD1D2D",
-            q :"나무",
-            req_type : "json",
-            sort : "dict"
-        },
-        withCredentials : true,
-        headers:{
-            'Access-Control-Allow-Origin': 'https://misssim29.github.io'
-        }
-        })
-        .then(function (res) {
-            console.log(red);
-        })
-        .catch(function (error) {
-            console.log(error);
-    });
+    fetchData();
 }
+
+
+const fetchData = async () => {
+    try {
+      const response = await axios.get('https://opendict.korean.go.kr/api/search', {
+        params: {
+          key: 'CA34B42695DBE19DDC617C4277B77376 ',
+          q: '나무',
+          sort: 'dict',
+          req_type : 'json',
+        },
+        withCredentials: true, // CORS 정책 처리를 위해 withCredentials 설정
+        headers: {
+          'Access-Control-Allow-Origin': 'https://misssim29.github.io' // 허용할 출처 설정
+        }
+      });
+  
+      // 데이터 처리
+      console.log(response.data);
+    } catch (error) {
+      // 오류 처리
+      console.error(error);
+    }
+  };
+  
