c33107f05f6297f9726314c26c65f9b90ab41200
심주연 <misssim29@naver.com>
2023-05-18 15:39:32 +0900

    .

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 
-const fetchData = async () => {
-    try {
-      const response = await axios.get('https://opendict.korean.go.kr/api/search', {
-        params: {
-          key: 'CA34B42695DBE19DDC617C4277B77376 ',
-          q: '나무',
-          sort: 'dict',
-          req_type : 'json',
-        },
-        withCredentials: true, // CORS 정책 처리를 위해 withCredentials 설정
-        headers: {
-          'Access-Control-Allow-Origin': 'https://misssim29.github.io' // 허용할 출처 설정
-        }
-      });
-  
-      // 데이터 처리
-      console.log(response.data);
-    } catch (error) {
-      // 오류 처리
-      console.error(error);
-    }
-};
 function addWord(){
-    fetchData();
+    fetch('https://krdict.korean.go.kr/api/search?key=E1269047A2AD9D16BB5CBEF9BA664EC9&q=나무&sort=dict', {
+  method: 'GET',
+  credentials: 'include', // CORS 정책 처리를 위해 credentials 설정
+  headers: {
+    'Access-Control-Allow-Origin': 'https://misssim29.github.io' // 허용할 출처 설정
+  }
+})
+  .then(response => {
+    if (response.ok) {
+      // 응답이 성공인 경우 처리
+      return response.json();
+    } else {
+      // 응답이 실패인 경우 오류 처리
+      throw new Error('Error: ' + response.status);
+    }
+  })
+  .then(data => {
+    // 데이터 처리
+    console.log(data);
+  })
+  .catch(error => {
+    // 오류 처리
+    console.error(error);
+  });
 }
