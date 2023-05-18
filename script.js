ddc7fed8a9e3462eea7fdc5920e08fd9f6fdb84d
심주연 <misssim29@naver.com>
2023-05-18 14:58:31 +0900

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //단어 입력
 function addWord(){
     axios({
-        method: 'get',
+        method: 'post',
         url:'https://opendict.korean.go.kr/api/search',
         params: {
             key : "7C2ADF260104EE196F0AF53D1BCE90ED",
